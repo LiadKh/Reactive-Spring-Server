3125236d288e87543f7ed014d0d5134f8732704e
Liad Khamdadash <liadkh95@gmail.com>
2020-01-18 15:09:25 +0200

    CRUD interface

---
+++ src/main/java/server/ReactiveRatingCrud.java
@@ -0,0 +1,24 @@
+package server;
+
+import java.time.Instant;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import reactor.core.publisher.Flux;
+
+public interface ReactiveRatingCrud extends ReactiveMongoRepository<Review, String> {
+	public Flux<Review> findAllByProductIdAndRatingGreaterThanEqual(String id, int rating, Sort sort);
+	public Flux<Review> findAllByProductIdAndRatingLessThanEqual(String id, int rating, Sort sort);
+	public Flux<Review> findAllByProductIdAndReviewTimestampAfter(String id, Instant Date, Sort sort);
+	public Flux<Review> findAllByProductIdAndReviewTimestampBefore(String id, Instant Date, Sort sort);
+	
+	public Flux<Review> findAllByCustomerEmailAndRatingGreaterThanEqual(String email, int rating, Sort sort);
+	public Flux<Review> findAllByCustomerEmailAndRatingLessThanEqual(String email, int rating, Sort sort);
+	public Flux<Review> findAllByCustomerEmailAndReviewTimestampAfter(String email, Instant Date, Sort sort);
+	public Flux<Review> findAllByCustomerEmailAndReviewTimestampBefore(String email, Instant Date, Sort sort);
+	
+	public Flux<Review> findAllByRatingBetween(int min, int max, Sort sort);
+	public Flux<Review> findAllByRatingBetweenAndReviewTimestampAfter(int min, int max, Instant Date, Sort sort);
+	public Flux<Review> findAllByRatingBetweenAndReviewTimestampBefore(int min, int max, Instant Date, Sort sort);
+}
